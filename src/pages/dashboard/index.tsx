cebe466e9a340dfc28367d10d03497a8c2ed0dcb
fgpsms <fgpsms@gmail.com>
2024-08-10 15:20:40 -0400

    Listando e Deletando os carros criados pelo usuario

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,23 @@
 import { useEffect, useState, useContext } from "react";
 import { Container } from "../../Components/Container";
 import { DashboardHeader } from "../../Components/PanelHeader";
-import { collection, getDocs, where, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  where,
+  doc,
+  deleteDoc,
+  query,
+} from "firebase/firestore";
 import { db } from "../../services/fbConect";
 import { AuthContext } from "../../contexts/AuthContexts";
 
 interface CarsProps {
   id: string;
-  name : string;
+  name: string;
   model: string;
   price: string | number;
-  year : string;
+  year: string;
   city: string;
   uid: string;
   km: string;
@@ -19,12 +26,13 @@ interface CarsProps {
 
 interface CarsImageProps {
   uid: string;
- name : string;
+  name: string;
   url: string;
 }
 
 export function Dashboard() {
   const [cars, setCars] = useState<CarsProps[]>([]);
+  const [loadingImages, setLoadingImages] = useState<string[]>([]);
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
@@ -60,49 +68,127 @@ export function Dashboard() {
     loadingCars();
   }, [user]);
 
+  const handleDeleteCars = async (id: string) => {
+    const delRef = doc(db, "cars", id);
+    await deleteDoc(delRef);
+    setCars(cars.filter((car) => car.id !== id));
+  };
+
+  const handleLoadingImages = (id: string) => {
+    setLoadingImages((prevImages) => [...prevImages, id]);
+  };
+
   return (
     <Container>
       <DashboardHeader />
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg relative">
-          <button
-            className="absolute bg-white w-10 h-10 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
-            onClick={() => {}}
+        {cars.map((itens) => (
+          <section
+            key={itens.id}
+            className="w-full bg-white rounded-lg relative"
           >
-            <svg
-              className="w-6 h-6 text-black dark:text-gray-800"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
+            <button
+              className="absolute bg-white w-10 h-10 rounded-full flex items-center justify-center right-2 top-2 drop-shadow"
+              onClick={() => {
+                handleDeleteCars(itens.id);
+              }}
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-              />
-            </svg>
-          </button>
-          <img
-            className="w-full rounded-lg mb-2 max-h-72"
-            src="https://firebasestorage.googleapis.com/v0/b/progweb-e5b15.appspot.com/o/images%2F7JtWS5QbaPgFxAg8mNQTZbwWYfo1%2F078b7b0e-ca9f-4e08-a25e-c1b217a7bb4a?alt=media&token=c40ece7e-c198-4b4a-a986-4c130165c756"
-          />
-          <p className="font-bold mt-1 px-2">NISSAN VERSA</p>
+              <svg
+                className="w-6 h-6 text-black dark:text-gray-800"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
+                />
+              </svg>
+            </button>
+            <div
+              role="status"
+              className="w-full rounded-lg mb-2 h-72 border-gray-200 shadow animate-pulse md:p-6 dark:border-gray-700 bg-slate-300"
+              style={{
+                display: loadingImages.includes(itens.id) ? "none" : "block",
+              }}
+            ></div>
+            <img
+              className="w-full rounded-lg mb-2 max-h-72"
+              src={itens.images[0].url}
+              onLoad={() => handleLoadingImages(itens.id)}
+              style={{
+                display: loadingImages.includes(itens.id) ? "block" : "none",
+              }}
+            />
+            <div
+              className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mt-1 mb-2 ml-2 px-2"
+              style={{
+                display: loadingImages.includes(itens.id) ? "none" : "block",
+              }}
+            ></div>
+            <p
+              className="font-bold mt-1 px-2"
+              style={{
+                display: loadingImages.includes(itens.id) ? "block" : "none",
+              }}
+            >
+              {itens.name}
+            </p>
 
-          <div className="flex flex-col px-2">
-            <span>Ano 2016/2016 | 230.000 KM</span>
-            <strong className="text-black font-bold mt-4">R$ 150.000</strong>
-          </div>
+            <div className="flex flex-col px-2">
+              <div
+                className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mt-1 mb-2 ml-2 px-2"
+                style={{
+                  display: loadingImages.includes(itens.id) ? "none" : "block",
+                }}
+              ></div>
+              <span
+                style={{
+                  display: loadingImages.includes(itens.id) ? "block" : "none",
+                }}
+              >
+                Ano {itens.year} | {itens.km} KM
+              </span>
+              <div
+                className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mt-1 mb-2 ml-2 px-2"
+                style={{
+                  display: loadingImages.includes(itens.id) ? "none" : "block",
+                }}
+              ></div>
+              <strong
+                style={{
+                  display: loadingImages.includes(itens.id) ? "block" : "none",
+                }}
+                className="text-black font-bold mt-4"
+              >
+                R$ {itens.price}
+              </strong>
+            </div>
 
-          <div className=" w-full h-px bg-slate-200 my-2"></div>
-          <div className="px-2 pb-2">
-            <span>CORUMBÁ - MS</span>
-          </div>
-        </section>
+            <div className=" w-full h-px bg-slate-200 my-2"></div>
+            <div
+              className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mt-1 mb-2 ml-2 px-2"
+              style={{
+                display: loadingImages.includes(itens.id) ? "none" : "block",
+              }}
+            ></div>
+            <div className="px-2 pb-2">
+              <span
+                style={{
+                  display: loadingImages.includes(itens.id) ? "block" : "none",
+                }}
+              >
+                {itens.city}
+              </span>
+            </div>
+          </section>
+        ))}
       </main>
     </Container>
   );
